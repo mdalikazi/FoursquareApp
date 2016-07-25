f12196e93419659a618aa9511f70be7c1090e223
Ali Kazi <Ali Kazi>
2016-07-26 09:56:02 +1000

    Resolve errors

--- app/src/main/java/exercise/foursquare/ali/foursquareapp/MainActivity.java
+++ app/src/main/java/exercise/foursquare/ali/foursquareapp/MainActivity.java
@@ -2,14 +2,15 @@ package exercise.foursquare.ali.foursquareapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestsProcessor mProcessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();*/
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mProcessor.getQuery(MainActivity.this, "coffee", 40.7f, -74f);
             }
         });
     }
--- app/src/main/java/exercise/foursquare/ali/foursquareapp/RequestsProcessor.java
+++ app/src/main/java/exercise/foursquare/ali/foursquareapp/RequestsProcessor.java
@@ -3,9 +3,11 @@ package exercise.foursquare.ali.foursquareapp;
 import android.content.Context;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.util.Log;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -14,42 +16,69 @@ import javax.net.ssl.HttpsURLConnection;
  * Created by kazi_ on 7/21/2016.
  */
 public class RequestsProcessor {
-
     //https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
-    private static final String URI = "https://api.foursquare.com/v2/venues/search?";
+    /*and if link is simple like location uri, for example geo:0,0?q=29203
+    Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q",29203).build();*/
     private static final String CLIENT_ID = "ACAO2JPKM1MXHQJCK45IIFKRFR2ZVL0QASMCBCG5NPJQWF2G";
     private static final String CLIENT_SECRET = "YZCKUYJ1WHUV2QICBXUBEILZI1DMPUIDP5SHV043O04FKBHL";
-    private static final int VERSION_PARAMTER = 20130815;
+    private static final String VERSION_PARAMTER = "20130815";
+    private static final String TAG = "Exceptions";
 
     private HttpsURLConnection mConnection;
-    private BufferedInputStream mInputStream;
-    private BufferedOutputStream mBufferedOutputStream;
+    private BufferedInputStream mBufferedInputStream;
+    private InputStreamReader mInputStreamReader;
+    private BufferedReader mBufferedReader;
+    private StringBuilder mResponseContent;
     private URL mUrl;
+    private int mResponseCode;
 
     public void getQuery(Context ctx, String query, float lat, float lang) {
         Resources res = ctx.getResources();
         String latLang = String.format(res.getString(R.string.param_lat_lang), lat, lang);
-        mUrl = new URL(
-                URI +
-                        res.getString(R.string.param_client_id) +
-                        res.getString(R.string.param_client_secret) +
-                        res.getString(R.string.param_version) +
-                        latLang +
-                        res.getString(R.string.param_query)
-
-        );
-
-        //https://www.myawesomesite.com/turtles/types?type=1&sort=relevance#section-name
-        //To build this with the Uri.Builder I would do the following.
-
         Uri.Builder builder = new Uri.Builder();
+
         builder.scheme("https")
-                .authority("www.myawesomesite.com")
-                .appendPath("turtles")
-                .appendPath("types")
-                .appendQueryParameter("type", "1")
-                .appendQueryParameter("sort", "relevance")
-                .fragment("section-name");
-        String myUrl = builder.build().toString();
+                .authority("api.foursquare.com")
+                .appendPath("v2")
+                .appendPath("venues")
+                .appendPath("search")
+                .appendQueryParameter("client_id", CLIENT_ID)
+                .appendQueryParameter("client_secret", CLIENT_SECRET)
+                .appendQueryParameter("v", VERSION_PARAMTER)
+                .appendQueryParameter("ll", latLang)
+                .appendQueryParameter("query", query);
+
+        try {
+            mUrl = new URL(builder.build().toString());
+            mConnection = (HttpsURLConnection) mUrl.openConnection();
+            mConnection.setDoOutput(false);
+            mConnection.setRequestMethod("GET");
+            mConnection.setConnectTimeout(15000);
+            mConnection.connect();
+            mResponseCode = mConnection.getResponseCode();
+
+            if(mResponseCode == 200) {
+                mBufferedInputStream = new BufferedInputStream(mConnection.getInputStream());
+                mInputStreamReader = new InputStreamReader(mBufferedInputStream);
+                mBufferedReader = new BufferedReader(mInputStreamReader);
+                mResponseContent = new StringBuilder();
+
+                String line;
+                while((line = mBufferedReader.readLine()) != null) {
+                    mResponseContent.append(line);
+                }
+                Log.d(TAG, "response content: " + mResponseContent);
+                mBufferedReader.close();
+                mInputStreamReader.close();
+                mBufferedInputStream.close();
+            }
+        } catch(Exception e) {
+            Log.d(TAG, "IO Exception: " + e);
+        } finally {
+            if(mConnection != null) {
+                mConnection.disconnect();
+            }
+        }
+        //return mResponseContent.toString();
     }
 }
