088cf095735d8eed115759971e4a060bd2fc643e
Ali Kazi <mdalikazi@gmail.com>
2017-07-06 09:41:58 +1000

    WIP adding searchView reveal with 2 toolbars

--- app/src/main/java/exercise/foursquare/ali/foursquareapp/main/AnimationUtils.java
+++ app/src/main/java/exercise/foursquare/ali/foursquareapp/main/AnimationUtils.java
@@ -2,6 +2,7 @@ package exercise.foursquare.ali.foursquareapp.main;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.view.View;
@@ -16,56 +17,43 @@ import exercise.foursquare.ali.foursquareapp.R;
 public class AnimationUtils {
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void circleReveal(int viewID, int posFromRight, boolean containsOverflow, final boolean isShow)
-    {
-        final View myView = findViewById(viewID);
+    public static void circleReveal(Context context, final View myView, int posFromRight, boolean containsOverflow, final boolean isShow) {
+        int width = myView.getWidth();
+        if(posFromRight > 0) {
+            width -= (posFromRight*context.getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material)) -
+                    (context.getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material) / 2);
+        }
 
-        int width=myView.getWidth();
+        if(containsOverflow) {
+            width -= context.getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_overflow_material);
+        }
 
-        if(posFromRight>0)
-            width<span class="pl-k">-=</span>(posFromRight<span class="pl-k">*</span>getResources()<span class="pl-k">.</span>getDimensionPixelSize(<span class="pl-smi">R</span><span class="pl-k">.</span>dimen<span class="pl-k">.</span>abc_action_button_min_width_material))<span class="pl-k">-</span>(getResources()<span class="pl-k">.</span>getDimensionPixelSize(<span class="pl-smi">R</span><span class="pl-k">.</span>dimen<span class="pl-k">.</span>abc_action_button_min_width_material)<span class="pl-k">/</span> <span class="pl-c1">2</span>);
-
-        if(containsOverflow)
-            width-=getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_overflow_material);
-
-        int cx=width;
-        int cy=myView.getHeight()/2;
+        int cx = width;
+        int cy = myView.getHeight() / 2;
 
         Animator anim;
-        if(isShow)
-            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0,(float)width);
-        else
-            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, (float)width, 0);
+        if(isShow) {
+            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, (float) width);
+        } else {
+            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, (float) width, 0);
+        }
 
-        anim.setDuration((long)220);
-
+        anim.setDuration(250);
         // make the view invisible when the animation is done
         anim.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                if(!isShow)
-                {
-                    super.onAnimationEnd(animation);
+                if(!isShow) {
                     myView.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
         // make the view visible and start the animation
-        if(isShow)
+        if(isShow) {
             myView.setVisibility(View.VISIBLE);
+        }
 
-        // start the animation
         anim.start();
-
-
     }
-    Use the below code you can show and hide the SearchView.
-// To reveal a previously invisible view
-
-            circleReveal(R.id.searchtoolbar,1,true,true);
-
-// To hide a previously visible view
-
-    circleReveal(R.id.searchtoolbar,1,true,false);
 }
--- app/src/main/java/exercise/foursquare/ali/foursquareapp/main/MainActivity.java
+++ app/src/main/java/exercise/foursquare/ali/foursquareapp/main/MainActivity.java
@@ -9,11 +9,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.transition.TransitionManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
@@ -29,8 +29,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -48,20 +47,30 @@ public class MainActivity extends AppCompatActivity {
 
     private double mUserLocationLat;
     private double mUserLocationLng;
-    private QueryService mQueryService;
-    private QueryResponse mQueryResponse;
-    private Gson mQueryResponseGsonObject;
-    private String mQueryResponseString;
+    private String mSearchQuery;
     private VenueAdapter mVenueAdapter;
     private FsLocationManager mLocationManager;
     private BroadcastReceiver mQueryBrodcastReceiver;
     private BroadcastReceiver mLocationBrodcastReceiver;
     private SimpleArrayMap<String, LinkedList> mVenues;
 
+    private QueryService mQueryService;
+    private QueryResponse mQueryResponse;
+    private Gson mQueryResponseGsonObject;
+    private String mQueryResponseString;
+
+    // Views
     private FloatingActionButton mLocationFab;
     private RecyclerView mRecyclerView;
     private Snackbar mSnackbar;
-    private LinearLayoutManager mLayoutManager;
+    private TextView mEmptyListMessage;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+        mLocationManager = new FsLocationManager(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,16 +81,20 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mLocationFab = (FloatingActionButton) findViewById(R.id.fab_location);
-        mRecyclerView = (RecyclerView) findViewById(R.id.venue_list_recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_activity_recycler_view);
+        mEmptyListMessage = (TextView) findViewById(R.id.main_activity_empty_message);
 
         mVenues = new SimpleArrayMap<>();
         mQueryService = new QueryService();
         mQueryResponse = new QueryResponse();
         mQueryResponseGsonObject = new Gson();
-        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mSnackbar = Snackbar.make(mLocationFab, "Getting your location...", Snackbar.LENGTH_INDEFINITE)
-                .setAction("Cancel", new View.OnClickListener() {
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        showEmptyMesssage(true);
+
+        mSnackbar = Snackbar.make(mLocationFab, getString(R.string.snackbar_message_getting_your_location), Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(R.string.cancel), new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         mSnackbar.dismiss();
@@ -93,9 +106,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mSnackbar.show();
-                if (hasLocationPermission()) {
-                    mLocationManager.connect();
-                }
+                checkLocationPermissionAndConnect();
             }
         });
 
@@ -117,7 +128,6 @@ public class MainActivity extends AppCompatActivity {
                 mUserLocationLng = intent.getDoubleExtra(AppConstants.USER_LOCATION_LNG, 0);
                 mQueryService.startQueryService(MainActivity.this, "coffee", mUserLocationLat, mUserLocationLng);
                 mSnackbar.dismiss();
-                Toast.makeText(MainActivity.this, "Your location is: " + mUserLocationLat + "," + mUserLocationLng, Toast.LENGTH_SHORT).show();
             }
         };
     }
@@ -134,11 +144,14 @@ public class MainActivity extends AppCompatActivity {
         return mVenues;
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(LOG_TAG, "onStart");
-        mLocationManager = new FsLocationManager(this);
+    private void showEmptyMesssage(boolean show) {
+        if (show) {
+            mEmptyListMessage.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+        } else {
+            mEmptyListMessage.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -175,31 +188,29 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                return false;
+                Log.d(LOG_TAG, "onQueryTextChange newText: " + query);
+                processSearch(query);
+                searchView.clearFocus();
+                return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                Log.d(LOG_TAG, "onQueryTextChange newText: " + newText);
+                // Disallow
                 return false;
             }
         });
 
-        /*LinearLayout searchBar = (LinearLayout) searchView.findViewById(R.id.search_bar);
-        LayoutTransition transition = new LayoutTransition();
-        transition.setDuration(1000);
-        searchBar.setLayoutTransition(transition);*/
-
         MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionExpand(MenuItem item) {
-                animateSearchView();
+                animateSearchView(true);
                 return true;
             }
 
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item) {
-                animateSearchView();
+                animateSearchView(false);
                 return true;
             }
         });
@@ -207,31 +218,42 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private void animateSearchView() {
-        TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.toolbar));
+    private void animateSearchView(boolean reveal) {
+        //TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.toolbar));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AnimationUtils.circleReveal(this, findViewById(R.id.search_view_reveal_toolbar), 1, true, reveal);
+        }
     }
 
-    public boolean hasLocationPermission() {
-        Log.i(LOG_TAG, "hasLocationPermission");
+    private void processSearch(String query) {
+        mSearchQuery = query;
+        if (mUserLocationLat == 0 || mUserLocationLng == 0) {
+            checkLocationPermissionAndConnect();
+        } else {
+            mQueryService.startQueryService(MainActivity.this, query, mUserLocationLat, mUserLocationLng);
+        }
+    }
+
+    public void checkLocationPermissionAndConnect() {
+        Log.i(LOG_TAG, "checkLocationPermissionAndConnect");
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
-            return true;
+            mLocationManager.connect();
         } else {
             if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
                 showLocationPermissionExplanation();
             } else {
                 requestLocationPermission();
             }
-            return false;
         }
     }
 
     private void showLocationPermissionExplanation() {
         Log.i(LOG_TAG, "showLocationPermissionExplanation");
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Please allow Location services to use this app.")
+        builder.setMessage(getString(R.string.permission_message_location))
                 .setCancelable(false)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         requestLocationPermission();
