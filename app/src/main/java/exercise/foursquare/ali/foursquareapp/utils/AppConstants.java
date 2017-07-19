60df7842bf5e96290aeb5a723725c730735cb7b6
Ali Kazi <mdalikazi@gmail.com>
2017-07-19 19:34:13 +1000

    Clean AppConstants. versionName -> 1.1, versionCode -> 2

--- app/src/main/java/exercise/foursquare/ali/foursquareapp/utils/AppConstants.java
+++ app/src/main/java/exercise/foursquare/ali/foursquareapp/utils/AppConstants.java
@@ -10,21 +10,6 @@ public class AppConstants {
     public static final String CLIENT_SECRET = "AKGCIU1ZSGLTF31R20O3DCUNJAT5IRMOPGPRK5FAEJ1LG4Y4";
     public static final String VERSION_PARAMTER = "20130815";
 
-    //Broadcast Intent extras
-    public static final String QUERY_RESPONSE = "queryResponse";
-
-    //Broadcast Intent filters
-    public static final String QUERY_COMPLETE = "queryComplete";
-
-    //Model getter helpers
-    public static final String VENUE_NAME = "venue_name";
-    public static final String VENUE_PHONE = "venue_phone";
-    public static final String VENUE_ADDRESS = "venue_address";
-    public static final String VENUE_LOCATION = "venue_location";
-    public static final String VENUE_DISTANCE = "venue_distance";
-    public static final String VENUE_HAS_MENU = "venue_has_menu";
-    public static final String VENUE_MENU_URL = "venue_menu_url";
-
     //Log tags
     public static final String LOG_TAG_QUERY = "Query";
 
